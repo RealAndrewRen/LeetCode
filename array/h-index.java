1f947f1b611ee6e8fedfabfac60d018cbfb4ad8b
Andrew Ren <108499792+RealAndrewRen@users.noreply.github.com>
2025-10-17 11:47:33 -0400

    LC-h-index [Runtime: 2 ms; Memory: 42.3 MB]

--- array/h-index.java
+++ array/h-index.java
@@ -1,12 +1,11 @@
 class Solution {
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
-        int h = 0;
         for (int i = 0; i < citations.length; i++){
-            if (citations[i] < citations.length && citations[i] <= citations[citations[i]]){
-                h = citations[i];
+            if (citations.length - i <= citations[i]){
+                return citations.length - i;
             }
         }
-        return h;
+        return 0;
     }
 }
