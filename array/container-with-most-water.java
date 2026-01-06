4011473dbba11c89e160b833b9b85fd92a696900
Andrew Ren <108499792+RealAndrewRen@users.noreply.github.com>
2026-01-05 20:53:15 -0500

    LC-container-with-most-water [Runtime: N/A; Memory: N/A]

---
+++ array/container-with-most-water.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int max = 0;
+        int start = 0;
+        int finish = height.length - 1;
+        for (int i = 0; i < height.length && start < finish; i++){
+            if ((finish - start) * Math.min(height[finish], height[start]) > max){
+                max = (finish - start) * Math.min(height[finish], height[start]);
+            }
+            start++;
+            finish--;
+        }
+        return max;
+    }
+}
