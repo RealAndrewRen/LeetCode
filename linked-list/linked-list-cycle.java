3173664a4ff1976adfd4578e3f101e2c732e9b9a
Andrew Ren <108499792+RealAndrewRen@users.noreply.github.com>
2026-01-06 22:34:24 -0500

    LC-linked-list-cycle [Runtime: 8 ms; Memory: 46.9 MB]

--- linked-list/linked-list-cycle.java
+++ linked-list/linked-list-cycle.java
@@ -11,7 +11,7 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if (head.next == null){
+        if (head == null){
             return false;
         }
         Set<ListNode> visited = new HashSet<>();
