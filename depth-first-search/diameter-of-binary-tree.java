40ea7cdc286d31c7e0392efad15fbdb06fb1e4c9
Andrew Ren <108499792+RealAndrewRen@users.noreply.github.com>
2026-01-06 21:59:38 -0500

    LC-diameter-of-binary-tree [Runtime: 0 ms; Memory: 47 MB]

---
+++ depth-first-search/diameter-of-binary-tree.java
@@ -0,0 +1,21 @@
+class Solution {
+    private int diameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return diameter;
+    }
+
+    private int height(TreeNode node) {
+        if (node == null) return 0;
+
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        // update global diameter
+        diameter = Math.max(diameter, leftHeight + rightHeight);
+
+        // return height of this subtree
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
