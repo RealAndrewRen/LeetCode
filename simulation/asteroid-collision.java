247504d5b924ca15eb6c92fdb8197b3086c3fab4
Andrew Ren <108499792+RealAndrewRen@users.noreply.github.com>
2026-01-08 20:34:00 -0500

    LC-asteroid-collision [Runtime: 6 ms; Memory: 47.2 MB]

--- simulation/asteroid-collision.java
+++ simulation/asteroid-collision.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         List<Integer> list = new ArrayList<Integer>();
+        List<Integer> negs = new ArrayList<Integer>();
         Stack<Integer> st = new Stack<Integer>();
         for (int i = 0; i < asteroids.length; i++){
             if (asteroids[i] >= 0){
@@ -16,18 +17,20 @@ class Solution {
                     continue;
                 }
                 if (st.isEmpty()){
-                    list.add(asteroids[i]);
+                    negs.add(asteroids[i]);
                 }
             }
         }
         while (!st.isEmpty()){
             list.add(st.pop());
         }
-        int result[] = new int[list.size()];
+        int result[] = new int[list.size() + negs.size()];
         for (int j = list.size() - 1; j >= 0; j--){
-            result[list.size() - 1 - j] = list.get(j);
+            result[negs.size() + list.size() - 1 - j] = list.get(j);
         }
-        Arrays.sort(result);
+        for (int i = 0; i < negs.size(); i++){
+            result[i] = negs.get(i);
+        }
         return result;
     }
 }
