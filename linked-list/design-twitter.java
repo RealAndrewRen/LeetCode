7e076dfb049a688d1b09b66bbfb20d5b8bb5feb1
Andrew Ren <108499792+RealAndrewRen@users.noreply.github.com>
2026-01-09 23:37:38 -0500

    LC-design-twitter [Runtime: 35 ms; Memory: 56.7 MB]

--- linked-list/design-twitter.java
+++ linked-list/design-twitter.java
@@ -1,58 +1,74 @@
 class Twitter {
-    private List<Integer> ids;
-    private Map<Integer, Integer> posts;
-    private Map<Integer, List<Integer>> following;
 
-    public Twitter() {
-        ids = new ArrayList<>();
-        posts = new HashMap<>();
-        following = new HashMap<>();
-    }
-    
-    public void postTweet(int userId, int tweetId) {
-        ensureUser(userId);
-        following.get(userId).add(userId); // follow self
-        posts.put(tweetId, userId);
-        ids.add(tweetId);
-    }
-    
-    public List<Integer> getNewsFeed(int userId) {
-    ensureUser(userId);
-
-    List<Integer> result = new ArrayList<>();
-
-    for (int i = ids.size() - 1; i >= 0 && result.size() < 10; i--) {
-        int tweetId = ids.get(i);
-        int author = posts.get(tweetId);
-
-        if (following.get(userId).contains(author)) {
-            result.add(tweetId); // append in recency order
+    private static class Tweet {
+        int id;
+        int time;
+        Tweet(int id, int time) {
+            this.id = id;
+            this.time = time;
         }
     }
-    return result;
-}
 
-    
+    private int time = 0;
+    private Map<Integer, Set<Integer>> following = new HashMap<>();
+    private Map<Integer, List<Tweet>> tweets = new HashMap<>();
+
+    public Twitter() {}
+
+    private void ensureUser(int userId) {
+        following.putIfAbsent(userId, new HashSet<>());
+        following.get(userId).add(userId); // self-follow
+        tweets.putIfAbsent(userId, new ArrayList<>());
+    }
+
+    public void postTweet(int userId, int tweetId) {
+        ensureUser(userId);
+        tweets.get(userId).add(new Tweet(tweetId, time++));
+    }
+
+    public List<Integer> getNewsFeed(int userId) {
+        ensureUser(userId);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> b[0] - a[0] // max-heap by time
+        );
+
+        // initialize heap with most recent tweet of each followed user
+        for (int followee : following.get(userId)) {
+            List<Tweet> list = tweets.get(followee);
+            if (!list.isEmpty()) {
+                int idx = list.size() - 1;
+                Tweet t = list.get(idx);
+                pq.offer(new int[]{t.time, t.id, followee, idx});
+            }
+        }
+
+        List<Integer> res = new ArrayList<>();
+
+        while (!pq.isEmpty() && res.size() < 10) {
+            int[] cur = pq.poll();
+            res.add(cur[1]);
+
+            int followee = cur[2];
+            int idx = cur[3] - 1;
+            if (idx >= 0) {
+                Tweet t = tweets.get(followee).get(idx);
+                pq.offer(new int[]{t.time, t.id, followee, idx});
+            }
+        }
+
+        return res;
+    }
+
     public void follow(int followerId, int followeeId) {
         ensureUser(followerId);
         ensureUser(followeeId);
         following.get(followerId).add(followeeId);
     }
-    
+
     public void unfollow(int followerId, int followeeId) {
-        ensureUser(followerId);
-        following.get(followerId).remove(Integer.valueOf(followeeId));
-    }
-    private void ensureUser(int userId) {
-        following.putIfAbsent(userId, new ArrayList<>());
+        if (followerId != followeeId && following.containsKey(followerId)) {
+            following.get(followerId).remove(followeeId);
+        }
     }
 }
-
-/**
- * Your Twitter object will be instantiated and called as such:
- * Twitter obj = new Twitter();
- * obj.postTweet(userId,tweetId);
- * List<Integer> param_2 = obj.getNewsFeed(userId);
- * obj.follow(followerId,followeeId);
- * obj.unfollow(followerId,followeeId);
- */
