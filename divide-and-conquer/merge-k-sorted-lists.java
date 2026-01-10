67e3b6997d516135826f72e0f0643d17caa05570
Andrew Ren <108499792+RealAndrewRen@users.noreply.github.com>
2026-01-10 11:25:09 -0500

    LC-merge-k-sorted-lists [Runtime: 5 ms; Memory: 46.7 MB]

---
+++ divide-and-conquer/merge-k-sorted-lists.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for (ListNode node : lists){
+            while (node != null){
+                heap.add(node.val);
+                node = node.next;
+            }
+        } 
+        if (heap.isEmpty()){
+            return null;
+        }
+        ListNode head = new ListNode(heap.poll());
+        ListNode curr = head;
+        while (!heap.isEmpty()){
+            curr.next = new ListNode(heap.poll());
+            curr = curr.next;
+        }
+        return head;
+    }
+}
