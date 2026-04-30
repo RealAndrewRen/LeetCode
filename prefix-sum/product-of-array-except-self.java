d06a18c1ee4f3c4e8a66d6a2bf0b32007c229741
Andrew Ren <108499792+RealAndrewRen@users.noreply.github.com>
2026-04-30 12:12:00 -0400

    LC-product-of-array-except-self [Runtime: 3 ms; Memory: 72.2 MB]

--- prefix-sum/product-of-array-except-self.java
+++ prefix-sum/product-of-array-except-self.java
@@ -1,20 +1,21 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int pre[] = new int[n];
-        int suff[] = new int[n];
-        pre[0] = 1;
-        suff[n - 1] = 1;
-        for (int i = 1; i < n; i++){
-            pre[i] = pre[i - 1] * nums[i - 1];
+        int[] res = new int[n];
+
+        // Step 1: prefix products
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
         }
-        for (int i = n - 2; i >= 0; i--){
-            suff[i] = suff[i + 1] * nums[i + 1];
+
+        // Step 2: suffix products
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
         }
-        int result[] = new int[n];
-        for (int i = 0; i < n; i++){
-            result[i] = pre[i] * suff[i];
-        }
-        return result;
+
+        return res;
     }
 }
