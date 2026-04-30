6fd853e6ad9675dcab840190b2876a04f3a9ea2a
Andrew Ren <108499792+RealAndrewRen@users.noreply.github.com>
2026-04-29 22:02:19 -0400

    LC-validate-binary-search-tree [Runtime: 2 ms; Memory: 45.3 MB]

--- depth-first-search/validate-binary-search-tree.java
+++ depth-first-search/validate-binary-search-tree.java
@@ -15,10 +15,19 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        if (root.left == null && root.right == null){
-            return true;
-        }
-
-        return root.right.val > root.val && root.left.val < root.val && isValidBST(root.left) && isValidBST(root.right);
-    }
+   if (root == null) return true;
+   Stack<TreeNode> stack = new Stack<>();
+   TreeNode pre = null;
+   while (root != null || !stack.isEmpty()) {
+      while (root != null) {
+         stack.push(root);
+         root = root.left;
+      }
+      root = stack.pop();
+      if(pre != null && root.val <= pre.val) return false;
+      pre = root;
+      root = root.right;
+   }
+   return true;
+}
 }
