0b04527fef33361e93454e45abf19628d94262ea
Andrew Ren <108499792+RealAndrewRen@users.noreply.github.com>
2026-04-29 21:10:52 -0400

    LC-remove-nth-node-from-end-of-list [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/remove-nth-node-from-end-of-list.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null || head.next == null && n == 1){
+            return null;
+        }
+        int size = 0;
+        ListNode curr = head;
+        while (curr != null){
+            size++;
+            curr = curr.next;
+        }
+        int index = size - n;
+        curr = head;
+        ListNode prev = null;
+        for (int i = 0; i < index; i++){
+            prev = curr;
+            curr = curr.next;
+        }
+        prev.next = curr.next;
+        return head;
+    }
+}
